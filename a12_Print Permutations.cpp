094302591bd8d4da4aadf705995a94400d492a9a
Mohd Faisal <faisal02v@gmail.com>
2023-03-13 18:12:21 +0530

    Update a12_Print Permutations.cpp

--- a12_Print Permutations.cpp
+++ a12_Print Permutations.cpp
@@ -29,3 +29,33 @@ void solve(vector<int>&ds, vector<int>arr, vector<bool>&flag, vector<vector<int>
 
 
 //link: https://www.codingninjas.com/codestudio/problems/758958?topList=striver-sde-sheet-problems&leftPanelTab=0
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    void solve(vector<int> nums,vector<vector<int>>&ans,int index){
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int j=index;j<nums.size();j++){
+            swap(nums[index],nums[j]);
+            solve(nums,ans,index+1);
+            swap(nums[index],nums[j]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector<vector<int>>ans;
+        int index=0;
+        solve(nums,ans,index);
+return ans;
+    }
+};
